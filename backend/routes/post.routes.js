ab43d19f5e3f5d1e7a16db14b0b23cb75fae3199
fred011 <fmmphahle01@gmail.com>
2025-02-10 12:51:41 +0200

    post controller done

---
+++ backend/routes/post.routes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { protectedRoute } from "../middleware/protectedRoute.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+  getAllPosts,
+  getFollowingPosts,
+  getLikedPosts,
+  getUserPosts,
+  likeUnlikePost,
+} from "../controllers/post.controller.js";
+
+const router = express.Router();
+
+router.get("/all", protectedRoute, getAllPosts);
+router.get("/following", protectedRoute, getFollowingPosts);
+router.get("/user/:username", protectedRoute, getUserPosts);
+router.get("/likes/:id", protectedRoute, getLikedPosts);
+router.post("/create", protectedRoute, createPost);
+router.post("/like/:id", protectedRoute, likeUnlikePost);
+router.post("/comment/:id", protectedRoute, commentOnPost);
+router.delete("/:id", protectedRoute, deletePost);
+
+export default router;
