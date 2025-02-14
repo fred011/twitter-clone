f66039d4fb7b5e5fefc71584ed9c70fd615f00ef
fred011 <fmmphahle01@gmail.com>
2025-02-14 13:13:12 +0200

    #

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend", "build")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
